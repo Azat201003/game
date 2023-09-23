da732459494edba63c0c1ba6fcb6a46fb9d76847
Azat201003 <zanelane87@gmail.com>
2023-09-23 12:18:23 +0400

    code

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -7,7 +7,7 @@ var speed = 75
 var right = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_animation_player.play("idle")
 
 func PlayerMovement(speed, delta):
 	if Input.is_action_pressed("moveRight"):
