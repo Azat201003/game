720fce3db671e14ef4417b2ba5e8cf0b9b769c89
Azat201003 <zanelane87@gmail.com>
2023-09-25 15:00:40 +0400

    many any think

--- scripts/animStart.gd
+++ scripts/animStart.gd
@@ -4,8 +4,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var _animation_player = $AnimatedSprite2D
-	await get_tree().create_timer(randi() % 10, false, false, true).timeout
 	if _animation_player != null:
 		_animation_player.play("idle")
 	else:
+		await get_tree().create_timer(randi() % 10, false, false, true).timeout
 		$g10.play("idle")
